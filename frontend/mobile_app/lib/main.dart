59d3e2e1fe9d3ff41e8c364f08e87d7bb8c3f2b7
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-03-24 23:17:45 +0700

    I don't know what's going on, but this is after generate the prisma...

--- frontend/mobile_app/lib/main.dart
+++ frontend/mobile_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/login.dart';
-import 'package:mobile_app/viewRecipe.dart';
+// import 'package:mobile_app/viewRecipe.dart';
 
 void main() {
   runApp(const MyApp());
