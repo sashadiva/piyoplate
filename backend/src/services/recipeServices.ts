9b76f2ead71dbc027f66aefbd352d833f5cfdb83
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-04-05 16:18:32 +0700

    fixing recipe upload

--- backend/src/services/recipeServices.ts
+++ backend/src/services/recipeServices.ts
@@ -1,19 +1,8 @@
+import { Prisma } from '@prisma/client';
 import { prisma } from '../lib/prisma';
 
-const createRecipe = async (data) => {
-  return await prisma.recipes.create({
-    data: {
-      author_id: data.author_id,
-      title: data.title,
-      description: data.description,
-      calories_per_serving: data.calories_per_serving,
-      cook_time_minutes: data.cook_time_minutes,
-      ingredients: data.ingredients, // String/JSON
-      instructions: data.instructions, // String/JSON
-      image_url: data.image_url,
-      video_url: data.video_url,
-    },
-  });
+const createRecipe = async (data: Prisma.recipesCreateInput) => {
+   return await prisma.recipes.create({ data });
 };
 
 const getAllRecipes = async () => {
@@ -28,7 +17,7 @@ const getAllRecipes = async () => {
   });
 };
 
-const getRecipeById = async (id) => {
+const getRecipeById = async (id: number) => {
   return await prisma.recipes.findUnique({
     where: { id: Number(id) },
     include: {
