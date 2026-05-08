6682a78887e5828a38187438855cd04466896fb9
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-05-08 14:04:51 +0700

    deleting all express backend

--- backend/src/routes/nutrisionsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { addDailyLog, getCalorieSummary } from '../controllers/nutrisionsControllers';
-
-const router = Router();
-
-router.post('/log', addDailyLog);
-router.get('/summary/:userId', getCalorieSummary);
-
-export default router;
