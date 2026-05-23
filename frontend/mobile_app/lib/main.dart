61a3c64f5e8d135a6b5a1bb651e59b9ed30746ae
Nadine Natasja Liu Sugandi <natasjanadine020726@gmail.com>
2026-05-23 21:27:47 +0700

    implementating firebase

--- frontend/mobile_app/lib/main.dart
+++ frontend/mobile_app/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/ui/screens/login.dart';
 // import 'package:mobile_app/viewRecipe.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Hidupkan Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
